e985759c3e667f22eced03bf97a7a11e75d82cd3
vegetason <irakozepirlo@gmail.com>
2024-01-08 16:26:52 +0200

    Finish the project's styles and script

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ let numberOfDivs;
 let numberButtons=document.querySelector('#numberOfDivs');
 let divArea;
 numberButtons.addEventListener('click',()=>{
-    divArea=1;
-    func();
-    numberOfDivs=parseInt(prompt('Enter number:'));
+    let div=document.querySelectorAll('.squareDiv');
+    if(div) div.forEach(n=>n.remove());
+    do{
+        numberOfDivs=parseInt(prompt('Enter number:'));
+    }
+    while(numberOfDivs>100)
     divArea=numberOfDivs*numberOfDivs;
-}
-)
-function func(){
-for(let i=0;i<divArea;i++){
-    squareDiv=document.createElement('div');
-    squareDiv.setAttribute('class','squareDiv');
-    container.appendChild(squareDiv);
-};
-let containerArea=640*640;
+    for(let i=0;i<divArea;i++){
+        squareDiv=document.createElement('div');
+        squareDiv.setAttribute('class','squareDiv');
+        container.appendChild(squareDiv);
+    }
+    let containerArea=640*640;
 answer=Math.sqrt(containerArea/divArea);
 let root=document.querySelector(':root');
 let rootStyles=getComputedStyle(root);
@@ -30,4 +30,6 @@ divs.forEach((div)=>{
         z=Math.floor(Math.random()*360)+1;
         div.style.cssText=`background:rgb(${x},${y},${z});`;
     });
-});}
+});
+}
+)
